710fee1ac51229376adb4fc0012b980c60707498
Douglas Medeiros <41749399+medeirosinacio@users.noreply.github.com>
2020-07-24 19:26:06 -0300

    upgrade alpine version to 12

--- scripts/updatesystem.sh
+++ scripts/updatesystem.sh
@@ -2,24 +2,25 @@
 
 if [ ! -f /var/updatesystem ]
 
-    then
+    	then
 
-    sudo su
+    	sudo su
 
-    source /var/hostvars
+    	source /var/hostvars
 
-	echo http://dl-3.alpinelinux.org/alpine/latest-stable/main > /etc/apk/repositories
-    echo http://dl-3.alpinelinux.org/alpine/latest-stable/community >> /etc/apk/repositories
+	echo http://dl-cdn.alpinelinux.org/alpine/latest-stable/community > /etc/apk/repositories
+	echo http://dl-cdn.alpinelinux.org/alpine/v3.12/main >> /etc/apk/repositories
+	echo http://dl-cdn.alpinelinux.org/alpine/v3.12/community >> /etc/apk/repositories
 
-    echo -e "${g}Configurando data e hora...${nc}"
+    	echo -e "${g}Configurando data e hora...${nc}"
 	rm -rf /usr/share/zoneinfo/*
 	apk add tzdata
 	cp /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime
 
-    echo -e "${g}Atualizando sistema...${nc}"
+    	echo -e "${g}Atualizando sistema...${nc}"
 	apk update
-    apk add --upgrade apk-tools
 	apk upgrade --available
+	sync
 
 	echo -e "${g}Reiniciando o sistema...${nc}"
 	touch /var/updatesystem
